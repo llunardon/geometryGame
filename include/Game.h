fcfe789c8bc6d312a330591eb527c6769960ca63
Lorenzo Lunardon <loud@gft.com>
2024-11-10 22:20:50 +0100

    File config description

--- include/Game.h
+++ include/Game.h
@@ -4,6 +4,46 @@
 #include <EntityManager.h>
 #include <SFML/Graphics.hpp>
 
+/*
+    config file:
+        - Window W H FL FS
+            - W, H: window width and height (int)
+            - FL: frame limit (int)
+            - FS: full screen or not (bool)
+
+        - Font F S R G B
+            - F: font file name, no whitespace (std::string)
+            - S: font size (int)
+            - R, G, B (int, int, int)
+
+        - Player SR CR S FR FG FB OR OG OB OT V
+            - SR: shape radius (int)
+            - CR: collision radius (int)
+            - S: speed, magnitude (float)
+            - FR, FG, FB: fill color (int, int, int)
+            - OR, OG, OB: outline color (int, int, int)
+            - OT: outline thickness (int)
+            - V: number of vertices of the shape (int)
+
+        - Enemy SR CR SMIN SMAX OR OG OB OT VMIN VMAX L SI
+            - SR: shape radius (int)
+            - CR: collision radius (int)
+            - SMIN, SMAX: speed min and max (float, float)
+            - OR, OG, OB: outline color (int, int, int)
+            - VMIN, VMAX: min and max vertices (int, int)
+            - L: small shape lifespan (int)
+
+         - Bullet: SR CR S FR FG FB OR OG OB OT V L
+            - SR: shape radius (int)
+            - CR: collision radius (int)
+            - S: speed, magnitude (float)
+            - FR, FG, FB: fill color (int, int, int)
+            - OR, OG, OB: outline color (int, int, int)
+            - OT: outline thickness (int)
+            - V: number of vertices of the shape (int)
+            - L: lifespan (int)
+*/
+
 struct PlayerConfig 
 {
     int SR, CR, FR, FG, FB, OR, OG, OB, OT, V;
